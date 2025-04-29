3d1811acd92d804b4ceef2c5b494e597d5491eb2
bfencil <133804481+bfencil@users.noreply.github.com>
2025-04-29 13:11:05 -0700

    Update sparse.py

--- ftle/flat/sparse.py
+++ ftle/flat/sparse.py
@@ -1,5 +1,5 @@
 
-from .utilities import plot_FTLE_2d, plot_FTLE_3d, interpolate, RK4_advection_2d
+from .utilities import plot_FTLE_2d, plot_FTLE_3d, interpolate, RK4_advection_2d, RK4_advection_3d
 from scipy.interpolate import LinearNDInterpolator
 import numpy as np
 from scipy.spatial import cKDTree
@@ -212,70 +212,7 @@ def FTLE_3d_sparse(
         interp_v = LinearNDInterpolator(velocity_points, velocity_vectors[:, 1], fill_value=0)
         interp_w = LinearNDInterpolator(velocity_points, velocity_vectors[:, 2], fill_value=0)
 
-        for t_index, _ in enumerate(fine_time):
-            x = trajectories[:, 0, t_index]
-            y = trajectories[:, 1, t_index]
-            z = trajectories[:, 2, t_index]
-
-            # RK4 steps
-            k1_x, k1_y, k1_z = interp_u(x, y, z), interp_v(x, y, z), interp_w(x, y, z)
-            k2_x, k2_y, k2_z = interp_u(x + 0.5 * dt * k1_x, y + 0.5 * dt * k1_y, z + 0.5 * dt * k1_z), \
-                               interp_v(x + 0.5 * dt * k1_x, y + 0.5 * dt * k1_y, z + 0.5 * dt * k1_z), \
-                               interp_w(x + 0.5 * dt * k1_x, y + 0.5 * dt * k1_y, z + 0.5 * dt * k1_z)
-            k3_x, k3_y, k3_z = interp_u(x + 0.5 * dt * k2_x, y + 0.5 * dt * k2_y, z + 0.5 * dt * k2_z), \
-                               interp_v(x + 0.5 * dt * k2_x, y + 0.5 * dt * k2_y, z + 0.5 * dt * k2_z), \
-                               interp_w(x + 0.5 * dt * k2_x, y + 0.5 * dt * k2_y, z + 0.5 * dt * k2_z)
-            k4_x, k4_y, k4_z = interp_u(x + dt * k3_x, y + dt * k3_y, z + dt * k3_z), \
-                               interp_v(x + dt * k3_x, y + dt * k3_y, z + dt * k3_z), \
-                               interp_w(x + dt * k3_x, y + dt * k3_y, z + dt * k3_z)
-
-            x_next = x + (dt / 6.0) * (k1_x + 2 * k2_x + 2 * k3_x + k4_x)
-            y_next = y + (dt / 6.0) * (k1_y + 2 * k2_y + 2 * k3_y + k4_y)
-            z_next = z + (dt / 6.0) * (k1_z + 2 * k2_z + 2 * k3_z + k4_z)
-
-            trajectories[:, 0, t_index + 1] = x_next
-            trajectories[:, 1, t_index + 1] = y_next
-            trajectories[:, 2, t_index + 1] = z_next
-
-    # --- Time-dependent advection ---
-    else:
-        for t_index, t in enumerate(fine_time):
-            t_floor = int(np.floor(t))
-            t_ceil = int(np.ceil(t))
-            t_fraction = t - t_floor  
-
-            # Interpolate velocity field at this fractional time
-            u_interp = interpolate(velocity_vectors[:, 0, t_floor], velocity_vectors[:, 0, t_ceil], t_fraction)
-            v_interp = interpolate(velocity_vectors[:, 1, t_floor], velocity_vectors[:, 1, t_ceil], t_fraction)
-            w_interp = interpolate(velocity_vectors[:, 2, t_floor], velocity_vectors[:, 2, t_ceil], t_fraction)
-
-            interp_u = LinearNDInterpolator(velocity_points, u_interp, fill_value=0)
-            interp_v = LinearNDInterpolator(velocity_points, v_interp, fill_value=0)
-            interp_w = LinearNDInterpolator(velocity_points, w_interp, fill_value=0)
-
-            x = trajectories[:, 0, t_index]
-            y = trajectories[:, 1, t_index]
-            z = trajectories[:, 2, t_index]
-
-            # RK4 steps
-            k1_x, k1_y, k1_z = interp_u(x, y, z), interp_v(x, y, z), interp_w(x, y, z)
-            k2_x, k2_y, k2_z = interp_u(x + 0.5 * dt * k1_x, y + 0.5 * dt * k1_y, z + 0.5 * dt * k1_z), \
-                               interp_v(x + 0.5 * dt * k1_x, y + 0.5 * dt * k1_y, z + 0.5 * dt * k1_z), \
-                               interp_w(x + 0.5 * dt * k1_x, y + 0.5 * dt * k1_y, z + 0.5 * dt * k1_z)
-            k3_x, k3_y, k3_z = interp_u(x + 0.5 * dt * k2_x, y + 0.5 * dt * k2_y, z + 0.5 * dt * k2_z), \
-                               interp_v(x + 0.5 * dt * k2_x, y + 0.5 * dt * k2_y, z + 0.5 * dt * k2_z), \
-                               interp_w(x + 0.5 * dt * k2_x, y + 0.5 * dt * k2_y, z + 0.5 * dt * k2_z)
-            k4_x, k4_y, k4_z = interp_u(x + dt * k3_x, y + dt * k3_y, z + dt * k3_z), \
-                               interp_v(x + dt * k3_x, y + dt * k3_y, z + dt * k3_z), \
-                               interp_w(x + dt * k3_x, y + dt * k3_y, z + dt * k3_z)
-
-            x_next = x + (dt / 6.0) * (k1_x + 2 * k2_x + 2 * k3_x + k4_x)
-            y_next = y + (dt / 6.0) * (k1_y + 2 * k2_y + 2 * k3_y + k4_y)
-            z_next = z + (dt / 6.0) * (k1_z + 2 * k2_z + 2 * k3_z + k4_z)
-
-            trajectories[:, 0, t_index + 1] = x_next
-            trajectories[:, 1, t_index + 1] = y_next
-            trajectories[:, 2, t_index + 1] = z_next
+     trajectories = RK4_advection_3d(velocity_points, velocity_vectors, trajectories, dt, fine_time, time_independent)
 
     # --- FTLE computation ---
     ftle = compute_Ftle_sparse(particle_positions, trajectories[:, :, -1], initial_time, final_time, neighborhood)
