d01ea22ec2b0b398ae9eb9802fbf55f5d31f9e05
bfencil <133804481+bfencil@users.noreply.github.com>
2025-04-29 13:04:39 -0700

    Update dense.py

--- ftle/flat/dense.py
+++ ftle/flat/dense.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.interpolate import LinearNDInterpolator
 from numba import njit
 import math
-from .utilities import plot_FTLE_2d, plot_FTLE_3d, interpolate
+from .utilities import plot_FTLE_2d, plot_FTLE_3d, interpolate, RK4_advection_2d
 
 
 
@@ -192,61 +192,7 @@ def FTLE_2d_dense(
 
     fine_time = fine_time[:-1]  # Exclude final point (we advect up to t_index + 1)
 
-    # --- Time-independent advection ---
-    if time_indepedent:
-        interp_u = LinearNDInterpolator(velocity_points, velocity_vectors[:, 0], fill_value=0)
-        interp_v = LinearNDInterpolator(velocity_points, velocity_vectors[:, 1], fill_value=0)
-
-        for t_index, _ in enumerate(fine_time):
-            x_curr = trajectories[:, 0, t_index]
-            y_curr = trajectories[:, 1, t_index]
-
-            # RK4 steps
-            k1_x, k1_y = interp_u(x_curr, y_curr), interp_v(x_curr, y_curr)
-            k2_x, k2_y = interp_u(x_curr + 0.5 * dt * k1_x, y_curr + 0.5 * dt * k1_y), \
-                         interp_v(x_curr + 0.5 * dt * k1_x, y_curr + 0.5 * dt * k1_y)
-            k3_x, k3_y = interp_u(x_curr + 0.5 * dt * k2_x, y_curr + 0.5 * dt * k2_y), \
-                         interp_v(x_curr + 0.5 * dt * k2_x, y_curr + 0.5 * dt * k2_y)
-            k4_x, k4_y = interp_u(x_curr + dt * k3_x, y_curr + dt * k3_y), \
-                         interp_v(x_curr + dt * k3_x, y_curr + dt * k3_y)
-
-            x_next = x_curr + (dt / 6.0) * (k1_x + 2*k2_x + 2*k3_x + k4_x)
-            y_next = y_curr + (dt / 6.0) * (k1_y + 2*k2_y + 2*k3_y + k4_y)
-
-            trajectories[:, 0, t_index + 1] = x_next
-            trajectories[:, 1, t_index + 1] = y_next
-
-    # --- Time-dependent advection ---
-    else:
-        for t_index, t in enumerate(fine_time):
-            t_floor = int(np.floor(t))
-            t_ceiling = int(np.ceil(t))
-            t_fraction = t - t_floor  
-
-            # Interpolate velocity at current time step
-            u_interp = interpolate(velocity_vectors[:, 0, t_floor], velocity_vectors[:, 0, t_ceiling], t_fraction)
-            v_interp = interpolate(velocity_vectors[:, 1, t_floor], velocity_vectors[:, 1, t_ceiling], t_fraction)
-
-            interp_u = LinearNDInterpolator(velocity_points, u_interp, fill_value=0)
-            interp_v = LinearNDInterpolator(velocity_points, v_interp, fill_value=0)
-
-            x_curr = trajectories[:, 0, t_index]
-            y_curr = trajectories[:, 1, t_index]
-
-            # RK4 steps
-            k1_x, k1_y = interp_u(x_curr, y_curr), interp_v(x_curr, y_curr)
-            k2_x, k2_y = interp_u(x_curr + 0.5 * dt * k1_x, y_curr + 0.5 * dt * k1_y), \
-                         interp_v(x_curr + 0.5 * dt * k1_x, y_curr + 0.5 * dt * k1_y)
-            k3_x, k3_y = interp_u(x_curr + 0.5 * dt * k2_x, y_curr + 0.5 * dt * k2_y), \
-                         interp_v(x_curr + 0.5 * dt * k2_x, y_curr + 0.5 * dt * k2_y)
-            k4_x, k4_y = interp_u(x_curr + dt * k3_x, y_curr + dt * k3_y), \
-                         interp_v(x_curr + dt * k3_x, y_curr + dt * k3_y)
-
-            x_next = x_curr + (dt / 6.0) * (k1_x + 2*k2_x + 2*k3_x + k4_x)
-            y_next = y_curr + (dt / 6.0) * (k1_y + 2*k2_y + 2*k3_y + k4_y)
-
-            trajectories[:, 0, t_index + 1] = x_next
-            trajectories[:, 1, t_index + 1] = y_next
+    trajectories = RK4_advection_2d(velocity_points, velocity_vectors, trajectories, dt, fine_time, time_independent)
 
     # --- Compute FTLE from reshaped results ---
     x_traj = trajectories[:, 0, :].reshape(x_dim1, x_dim2, fine_time_length)
